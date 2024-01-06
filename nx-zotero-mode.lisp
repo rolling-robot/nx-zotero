8d687c019ca3de3a234f7da783d4c8f21de42b79
Petr Mukhachev <mukhachev@phystech.edu>
2024-01-06 22:11:01 +0300

    improve documentation

--- nx-zotero-mode.lisp
+++ nx-zotero-mode.lisp
@@ -25,6 +25,7 @@
    ))
 
 (defun api-save-page (url html cookie translator-id)
+  "Send a save page request to zotero connector api. Returns the result decoded from json to lisp structures."
   (multiple-value-bind (body status)
       (drakma:http-request "http://localhost:23119/connector/savePage"
                            :method :post
@@ -34,15 +35,16 @@
                                                    (cons 'uri (quri:render-uri url))
                                                    (cons 'cookie cookie)
                                                    (cons 'translator-+id+ translator-id))))
-;    (echo (format nil "Received: ~a" (flexi-streams:octets-to-string body)))
+    (echo (format nil "Received: ~a" (flexi-streams:octets-to-string body)))
     (values (njson:decode (flexi-streams:octets-to-string body)) status)))
 
 (defun api-select-items (selected-items instance-id)
+  "Send a list of selected items to zotero connector api in order to save them. Returns status of the request only."
   (let ((content (njson:encode (list
                                 (cons 'selected-items (loop for item in selected-items
                                                             collect (cons (url item) (name item))))
                                 (cons 'instance-+ID+ instance-id)))))
-;    (echo (format nil "Sending ~a" content))
+    (echo (format nil "Sending ~a" content))
     (multiple-value-bind (body status)
         (drakma:http-request "http://localhost:23119/connector/selectItems"
                              :method :post
@@ -51,10 +53,13 @@
       status)))
 
 (defun api-ping ()
-    (drakma:http-request "http://localhost:23119/connector/ping"
-                         :method :get))
+  "Ping zotero api"
+  (drakma:http-request "http://localhost:23119/connector/ping"
+                       :method :get))
 
 (defun api-detect (url html cookie)
+  "\"Detect\" endpoint detects whether there is an available translator
+to handle a given page. Returns a list of available translators as a list."
   (multiple-value-bind (body status)
       (drakma:http-request "http://localhost:23119/connector/detect"
                            :method :post
@@ -67,13 +72,12 @@
     (njson:decode (flexi-streams:octets-to-string body))))
 
 (defun detect (buffer)
+  "Returns a list of available translators for a page in a given buffer. Not yet used."
   (let ((page-source (nyxt/mode/document:get-url-source (nyxt:url buffer))))
     (api-detect (nyxt:url buffer) page-source nil)))
 
-(defun select-items (items)
-  (format t "~a" items))
-
 (defun zotero-send-buffer (buffer)
+  "Sends buffer source to zotero api for saving and returns results"
   (let ((page-source (nyxt/mode/document:get-url-source (nyxt:url buffer))))
     (api-save-page (nyxt:url buffer) page-source nil nil)))
 
@@ -97,6 +101,8 @@
                                                        collect (make-instance 'zotero-option :name (njson:jget url items) :url url))))))
 
 (define-command save-current ()
+  "Sends current buffer to zotero api and handles the result: in case
+there are multiple papers to save, provides an interface to select one of them"
   (multiple-value-bind (ans status) (zotero-send-buffer (nyxt:current-buffer))
     (let ((result
             (case status
