8c0ea1c800f73b41ea5254f6eab578df0e4d39cb
Petr Mukhachev <mukhachev@phystech.edu>
2024-01-07 12:52:28 +0300

    use defined mode configuration

--- nx-zotero-mode.lisp
+++ nx-zotero-mode.lisp
@@ -15,19 +15,22 @@
 
 (define-mode zotero-mode ()
   "A mode for extracting metadata from a web page and sending it to zotero."
-  ((zotero-port :type integer
-                :initform 23119
-                :documentation "Port number to connect to Zotero instance.")
+  ((zotero-host :type string
+                :initform "localhost:23119"
+                :documentation "Host and port number to connect to Zotero instance.")
+   (ping-timeout :type float
+                 :initform 0.1
+                 :documentation "Ping timeout in seconds")
    (keyscheme-map
     (define-keyscheme-map "zotero-mode" ()
       nyxt/keyscheme:emacs
       (list "C-c z" 'zotero-save-current)))
    ))
 
-(defun api-save-page (url html cookie translator-id)
+(defun api-save-page (host url html cookie translator-id)
   "Send a save page request to zotero connector api. Returns the result decoded from json to lisp structures."
   (multiple-value-bind (body status)
-      (drakma:http-request "http://localhost:23119/connector/savePage"
+      (drakma:http-request (str:concat "http://" host "/connector/savePage")
                            :method :post
                            :content-type "application/json"
                            :content (njson:encode (list
@@ -35,10 +38,9 @@
                                                    (cons 'uri (quri:render-uri url))
                                                    (cons 'cookie cookie)
                                                    (cons 'translator-+id+ translator-id))))
-    (echo (format nil "Received: ~a" (flexi-streams:octets-to-string body)))
     (values (njson:decode (flexi-streams:octets-to-string body)) status)))
 
-(defun api-select-items (selected-items instance-id)
+(defun api-select-items (host selected-items instance-id)
   "Send a list of selected items to zotero connector api in order to save them. Returns status of the request only."
   (let ((content (njson:encode (list
                                 (cons 'selected-items (loop for item in selected-items
@@ -46,25 +48,40 @@
                                 (cons 'instance-+ID+ instance-id)))))
     (echo (format nil "Sending ~a" content))
     (multiple-value-bind (body status)
-        (drakma:http-request "http://localhost:23119/connector/selectItems"
+        (drakma:http-request (str:concat "http://" host "/connector/selectItems")
                              :method :post
                              :content-type "application/json"
                              :content content)
+      (declare (ignore body))
       status)))
 
-(defun api-ping ()
+(defun api-ping (host timeout)
   "Ping zotero api. Returns status returned by api or throws a condition usocket:timeout-error"
   (multiple-value-bind (ans status)
-      (drakma:http-request "http://localhost:23119/connector/ping"
+      (drakma:http-request (str:concat "http://" host "/connector/ping")
                            :method :get
-                           :connection-timeout 0.1)
+                           :connection-timeout timeout)
+    (declare (ignore ans))
     status))
 
-(defun api-detect (url html cookie)
+(defun current-zotero-mode ()
+  "Return `zotero-mode' if it's active in the current buffer."
+  (alex:when-let ((mode (sym:resolve-symbol :zotero-mode :mode '(:nx-zotero-mode))))
+    (find-submode mode)))
+
+(defun ping-p ()
+  (with-slots (zotero-host ping-timeout) (current-zotero-mode)
+    (if (handler-case (api-ping zotero-host ping-timeout)
+          (usocket:timeout-error ()
+                                 nil))
+        t
+      nil)))
+
+(defun api-detect (host url html cookie)
   "\"Detect\" endpoint detects whether there is an available translator
 to handle a given page. Returns a list of available translators as a list."
   (multiple-value-bind (body status)
-      (drakma:http-request "http://localhost:23119/connector/detect"
+      (drakma:http-request (str:concat "http://" host "/connector/detect")
                            :method :post
                            :content-type "application/json"
                            :content (njson:encode (list
@@ -72,17 +89,20 @@ to handle a given page. Returns a list of available translators as a list."
                                                    (cons 'uri (quri:render-uri url))
                                                    (cons 'cookie cookie)))
                            )
+    (declare (ignore status))
     (njson:decode (flexi-streams:octets-to-string body))))
 
 (defun detect (buffer)
   "Returns a list of available translators for a page in a given buffer. Not yet used."
   (let ((page-source (nyxt/mode/document:get-url-source (nyxt:url buffer))))
-    (api-detect (nyxt:url buffer) page-source nil)))
+    (api-detect (zotero-host (current-zotero-mode))
+                (nyxt:url buffer) page-source nil)))
 
 (defun zotero-send-buffer (buffer)
   "Sends buffer source to zotero api for saving and returns results"
   (let ((page-source (nyxt/mode/document:get-url-source (nyxt:url buffer))))
-    (api-save-page (nyxt:url buffer) page-source nil nil)))
+    (api-save-page (zotero-host (current-zotero-mode))
+                   (nyxt:url buffer) page-source nil nil)))
 
 (define-class zotero-option ()
   ((name :initarg :name :accessor name)
@@ -106,14 +126,13 @@ to handle a given page. Returns a list of available translators as a list."
 (define-command zotero-save-current ()
   "Sends current buffer to zotero api and handles the result: in case
 there are multiple papers to save, provides an interface to select one of them"
-  (if (handler-case (api-ping)
-        (usocket:timeout-error (var)
-                               nil))
+  (if (ping-p)
       (multiple-value-bind (ans status) (zotero-send-buffer (nyxt:current-buffer))
         (let ((result
                (case status
                  (300 (let ((selected (select-one ans)))  ;; Several items available. Select one.
-                        (api-select-items selected (njson:jget "instanceID" ans))))
+                        (api-select-items (zotero-host (current-zotero-mode))
+                                          selected (njson:jget "instanceID" ans))))
                  (201 201)
                  ((t) status))))
           (case result
@@ -121,5 +140,7 @@ there are multiple papers to save, provides an interface to select one of them"
             (500 (echo (format nil "Error ~a" result)))
             )
           result))
-    (echo "Ping timeout. Check that Zotero is running.")
+    (progn
+      (echo "Ping timeout. Check that Zotero is running.")
+      nil)
     ))
